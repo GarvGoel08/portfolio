054bedea3f2475ce2113d7cf46a59d3be1e4a7d0
Garv Goel <garvgoel2927@gmail.com>
2024-04-06 08:37:55 +0530

    Responsiveness Fixes

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -3,20 +3,20 @@ import Navbar from "../Navbar/Navbar";
 import { useEffect, useRef, useState } from "react";
 import "./Hero.css";
 
-
 export default function Hero() {
-    const heroRef = useRef(null);
-    const [topMargin, setTopMargin] = useState(0);
-  
-    useEffect(() => {
-      if (heroRef.current) {
-        const rect = heroRef.current.getBoundingClientRect();
-        const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-        const absoluteTop = rect.top + scrollTop;
-  
-        setTopMargin(absoluteTop);
-      }
-    }, []);
+  const heroRef = useRef(null);
+  const [topMargin, setTopMargin] = useState(0);
+
+  useEffect(() => {
+    if (heroRef.current) {
+      const rect = heroRef.current.getBoundingClientRect();
+      const scrollTop =
+        window.pageYOffset || document.documentElement.scrollTop;
+      const absoluteTop = rect.top + scrollTop;
+
+      setTopMargin(absoluteTop);
+    }
+  }, []);
   return (
     <div className="HeroMain">
       <Navbar />
@@ -29,16 +29,18 @@ export default function Hero() {
       <div className="Hero">
         <div className="HeroText" ref={heroRef}>
           <div className="sectionHead">{"< Start />"}</div>
-          <div className="flex flex-row" style={{flexWrap: 'wrap'}}>
+          <div className="flex flex-row" style={{ flexWrap: "wrap", gap: '8px' }}>
             <h1 className="NameHeading">Hi, my name is </h1>
-            <h1 className="Name Name-Start NameHeading">G</h1>
-            <h1 className="Name NameHeading">a</h1>
-            <h1 className="Name NameHeading">r</h1>
-            <h1 className="Name NameHeading">v</h1>
-            <h1 className="Name Name-Start NameHeading">G</h1>
-            <h1 className="Name NameHeading">o</h1>
-            <h1 className="Name NameHeading">e</h1>
-            <h1 className="Name NameHeading">l</h1>
+            <div className="flex flex-row">
+              <h1 className="Name NameHeading">G</h1>
+              <h1 className="Name NameHeading">a</h1>
+              <h1 className="Name NameHeading">r</h1>
+              <h1 className="Name NameHeading">v</h1>
+              <h1 className="Name Name-Start NameHeading">G</h1>
+              <h1 className="Name NameHeading">o</h1>
+              <h1 className="Name NameHeading">e</h1>
+              <h1 className="Name NameHeading">l</h1>
+            </div>
           </div>
           <h2 className="HeroSkills">I am a Full Stack Developer</h2>
           <div className="KnowMore">Wanna Know More about me? Lets go..</div>
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,15 +1,41 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./Navbar.css";
 
 export default function Navbar() {
+  const [navbar, setNavbar] = useState(false);
   return (
-    <div className="Navbar">
-      <div className="NavbarLogo">
-        <span className="bracket">&lt; </span>
-        <div className="NavbarLogoText">Garv</div>
-        <span className="bracket"> /&gt;</span>
+    <div className={`Navbar ${navbar ? "Navbar-Show MobileNav" : ""}`}>
+      <div className="MobNav">
+        <div className="NavbarLogo">
+          <span className="bracket">&lt; </span>
+          <div className="NavbarLogoText">Garv</div>
+          <span className="bracket"> /&gt;</span>
+        </div>
+        <button
+          className="NavbarToggle"
+          onClick={() => {
+            setNavbar(!navbar);
+          }}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="feather feather-menu"
+          >
+            <line x1="3" y1="12" x2="21" y2="12"></line>
+            <line x1="3" y1="6" x2="21" y2="6"></line>
+            <line x1="3" y1="18" x2="21" y2="18"></line>
+          </svg>
+        </button>
       </div>
-      <div className="NavbarLinks">
+      <div className={`NavbarLinks ${navbar ? "Navbar-Show" : ""}`}>
         <div className="NavbarLink NavbarLinkActive">
           <span className="bracket-nav">&lt; </span>
           <span>Home</span>
