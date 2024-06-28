d23f551be16df9fa38835a489368f55d7108e9d9
GarvGoel08 <121159732+GarvGoel08@users.noreply.github.com>
2024-06-28 15:55:53 +0530

    Added Ez-Interview to Projects

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -13,9 +13,9 @@ export default function Projects() {
         <div className="ProjectsHeading">My Web Projects..</div>
         {/* Project Card */}
         <div className="projectsList">
-          <ProjectCard title="Coming Soon" description="An app to innovate interviews is coming soon.. Stay Tuned." image="/Images/ComingSoonEZ.png"/>
-          <ProjectCard link="https://taskongo.vercel.app/" title="TaskOnGo" description="Create and manage your tasks with TaskOnGo" image="/Images/TOG_Logo.png"/>
+          <ProjectCard link="https://ezinterview.vercel.app" title="Ez-Interview" description="A revolutionary website to prevent cheating during hiring." image="/Images/EzInerviewLogo.jpeg"/>
           <ProjectCard link="https://item-zilla-frontend.vercel.app/" title="ItemZilla" description="ecommerce website made by utilizing RazorPay payment" image="/Images/ItemZilla.png"/>
+          <ProjectCard link="https://taskongo.vercel.app/" title="TaskOnGo" description="Create and manage your tasks with TaskOnGo" image="/Images/TOG_Logo.png"/>
           <ProjectCard link="https://sheets-theta.vercel.app/" title="Docs" description="Website to collaboratively share documents" image="/Images/Docs.png"/>
         </div>
       </div>
