5819f3282a7a71478af55064e48a90d392ab5638
Garv Goel <garvgoel2927@gmail.com>
2025-09-01 20:13:46 +0530

    Added Labiotic to Projects

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,4 +1,4 @@
-import React from 'react'
+zzimport React from 'react'
 import ProjectCard from '../ProjectCard/ProjectCard'
 import './Projects.css'
 
@@ -15,6 +15,7 @@ export default function Projects() {
         <div className="projectsList">
           <ProjectCard link="https://devicode.vercel.app/" title="Devicode" description="A No Code Backend tool for developers." image="/Images/Devicode.jpg"/>
           <ProjectCard link="https://ezinterview.vercel.app" title="Ez-Interview" description="A revolutionary website to prevent cheating during hiring." image="/Images/EzInerviewLogo.jpeg"/>
+          <ProjectCard link="https://labiotic.vercel.app/" title="Labiotic" description="AI-Powered tool that automates the process of creating lab reports." image="/Images/LabioticsLogo.png"/>
           <ProjectCard link="https://docongoai.vercel.app/" title="Docongo" description="A doctor powered by AI available for you at all times" image="/Images/DocOnGoLogo.png"/>
           <ProjectCard link="https://dev-links-one-rust.vercel.app" title="DevLinks" description="A platform for developers to share their social links." image="/Images/DevLinks.png"/>
           <ProjectCard link="https://item-zilla-frontend.vercel.app/" title="ItemZilla" description="ecommerce website made by utilizing RazorPay payment" image="/Images/ItemZilla.png"/>
